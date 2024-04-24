f5aa175737e2cf3baf5b2fa2f616a7f6a87a8178
yeinous <yeinous@outlook.com>
2024-04-24 19:44:04 +0800

    fix: 修改yml文件

--- src/components/form.tsx
+++
@@ -1,8 +0,0 @@
-export default function Form() {
-  return (
-    <form>
-      <textarea name="content" placeholder="Start writing..." />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
