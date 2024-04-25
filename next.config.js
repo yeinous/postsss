cad0359b90d6e290d0ca918f7f1b20383ee60521
yeinous <yeinous@outlook.com>
2024-04-25 15:26:20 +0800

    chore: 配置文件修改

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
-  assetPrefix: isProd ? "/yeinous" : "",
+  basePath: isProd ? "/postsss" : "",
   images: {
     remotePatterns: [
       {
