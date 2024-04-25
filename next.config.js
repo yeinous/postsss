ee167f1cfd874d6fbcbad8f6941baf015b3678d6
yeinous <yeinous@outlook.com>
2024-04-25 15:35:26 +0800

    chore: 修改为assetPrefix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/postsss" : "",
+  assetPrefix: isProd ? "/postsss" : "",
   images: {
     remotePatterns: [
       {
