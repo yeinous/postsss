81e4a281712292baaf3bd85994e98abe95a37805
yeinous <yeinous@outlook.com>
2024-04-25 16:48:42 +0800

    fix: 另一个尝试

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -6,9 +6,9 @@ export default async function PostsPage() {
     <main className="text-center pt-16 px-5">
       <h1 className="text-5xl font-semibold mb-7">All posts</h1>
 
-      {/* <Suspense fallback="Loading...">
+      <Suspense fallback="Loading...">
         <PostsList />
-      </Suspense> */}
+      </Suspense>
     </main>
   );
 }
