8f6fa703110f8705426e15b711ed8a0eb528c086
yeinous <yeinous@outlook.com>
2024-04-25 16:38:20 +0800

    Revert "chore: 添加basePath"
    This reverts commit 6bedd84d5c9d0f6617a12d290b14bbcddb286b27.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   basePath: isProd ? "/postsss" : "",
-  // assetPrefix: isProd ? "/postsss/" : "",
+  assetPrefix: isProd ? "/postsss/" : "",
   images: {
     remotePatterns: [
       {
