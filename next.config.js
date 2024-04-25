daa99ab74fcae8e0780213b23469281ebad4b98d
yeinous <yeinous@outlook.com>
2024-04-25 14:31:01 +0800

    chore: 配置文件修改

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
+  basePath: isProd ? "/yeinous" : "",
   images: {
     remotePatterns: [
       {
