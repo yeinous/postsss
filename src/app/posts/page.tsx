7d1c22bd67b86c00f0cd1b5b1a6c2d8179f7406d
yeinous <yeinous@outlook.com>
2024-04-25 16:40:25 +0800

    fix: 一个尝试

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,15 +2,13 @@ import PostsList from "@/components/posts-list";
 import { Suspense } from "react";
 
 export default async function PostsPage() {
-
-
   return (
     <main className="text-center pt-16 px-5">
       <h1 className="text-5xl font-semibold mb-7">All posts</h1>
 
-      <Suspense fallback="Loading...">
+      {/* <Suspense fallback="Loading...">
         <PostsList />
-      </Suspense>
+      </Suspense> */}
     </main>
   );
 }
