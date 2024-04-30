1e32e9741825e26a96d36b7ed4232842caccf546
yeinous <yeinous@outlook.com>
2024-04-30 17:46:16 +0800

    docs: Dockerfile文件修正完成

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  output: "export",
+  output: "standalone",
   basePath: isProd ? "/postsss" : "",
   assetPrefix: isProd ? "/postsss/" : "",
   images: {
